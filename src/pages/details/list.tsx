74ff261ae9d5d79012df0a993641853f07970670
henery <henery_002@163.com>
2025-03-20 17:52:11 +0800

    fix: update.

--- src/pages/details/list.tsx
+++ src/pages/details/list.tsx
@@ -6,34 +6,32 @@ import styles from './details.less'
 export default function ListPage() {
 	return (
 		<View className={styles.listPagePage}>
-			<View>
-				<AtSearchBar
-					value=""
-					placeholder="搜索你感兴趣的内容"
-					onChange={() => null}
-				/>
-			</View>
-			<View>
-				<AtTabs
-					scroll
-					current={0}
-					tabList={
-						[
-							{ title: '标签页1' },
-							{ title: '标签页2' },
-							{ title: '标签页3' },
-							{ title: '标签页4' },
-							{ title: '标签页5' },
-							{ title: '标签页6' }
-						]}
-					onClick={() => null}
-				>
-					{
-						[1, 2, 3].map(v => <AtTabsPane current={1} index={0}><View>11</View></AtTabsPane>)
-					}
-				</AtTabs>
-			</View>
-			<View>3</View>
+			<AtSearchBar
+				value=""
+				placeholder="搜索你感兴趣的内容"
+				onChange={() => null}
+			/>
+			<AtTabs
+				scroll
+				current={0}
+				tabList={
+					[
+						{ title: '推荐' },
+						{ title: '旅行' },
+						{ title: '美食' },
+						{ title: '摄影' },
+						{ title: '户外' },
+						{ title: '穿搭' },
+						{ title: '家居' },
+						{ title: '职场' },
+						{ title: '音乐' }
+					]}
+				onClick={() => null}
+			>
+				{
+					[1, 2, 3].map(v => <AtTabsPane current={0} index={0}><View>11</View></AtTabsPane>)
+				}
+			</AtTabs>
 		</View>
 	)
 }
